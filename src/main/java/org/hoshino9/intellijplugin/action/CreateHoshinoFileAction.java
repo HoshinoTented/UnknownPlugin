4a2fdd4899f358fb819f37ef7919389efbd761ca
HoshinoTented <limbolrain@gmail.com>
2018-08-24 21:48:09 +0800

    Unknown

--- src/main/java/org/hoshino9/intellijplugin/action/CreateHoshinoFileAction.java
+++ src/main/java/org/hoshino9/intellijplugin/action/CreateHoshinoFileAction.java
@@ -1,10 +1,11 @@
 package org.hoshino9.intellijplugin.action;
 
+import com.intellij.ide.actions.CreateFileAction;
 import org.hoshino9.intellijplugin.HoshinoIcons;
 import org.hoshino9.intellijplugin.language.HoshinoLanguage;
 import org.jetbrains.annotations.Nullable;
 
-public class CreateHoshinoFileAction extends com.intellij.ide.actions.CreateFileAction {
+public class CreateHoshinoFileAction extends CreateFileAction {
 	public CreateHoshinoFileAction() {
 		super("Hoshino File", "Create a Hoshino File", HoshinoIcons.hoshino);
 	}
